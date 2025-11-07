6c61b409dd617a78ffcdd5b159e979a7bea8d8cd
zeltina99 <minec369@outlook.com>
2025-11-07 16:52:40 +0900

    커브로 위 아래 움직임

--- Source/Zeltina_UnrealCPP/Private/Test/FloatingActor_Curve.cpp
+++ Source/Zeltina_UnrealCPP/Private/Test/FloatingActor_Curve.cpp
@@ -3,3 +3,26 @@
 
 #include "Test/FloatingActor_Curve.h"
 
+void AFloatingActor_Curve::OnFloatingMeshUpdate(float DeltaTime)
+{
+	if (HeightCurve)
+	{
+		// 커브가 있음
+		ElapsedTime += DeltaTime;
+
+		float curveInput = FMath::Fmod(ElapsedTime / Duration, 1.0f);	// Duration을 이용해 한 주기 시간 결정, 0.0 ~ 1.0범위의 숫자로 반복
+		float curveValue = HeightCurve->GetFloatValue(curveInput);		// 0 ~ 1 ~ 0
+
+		UE_LOG(LogTemp, Log, TEXT("Input : %.2f"), curveInput);
+		UE_LOG(LogTemp, Log, TEXT("Height : %.2f"), curveValue);
+
+		BodyMesh->SetRelativeLocation(FVector(0, 0, curveValue * MoveHeight));
+		BodyMesh->AddRelativeRotation(FRotator(0, SpinSpeed * DeltaTime, 0));
+	}
+	else
+	{
+		// 커브가 없음
+		Super::OnFloatingMeshUpdate(DeltaTime);
+	}
+
+}
--- Source/Zeltina_UnrealCPP/Public/Test/FloatingActor_Curve.h
+++ Source/Zeltina_UnrealCPP/Public/Test/FloatingActor_Curve.h
@@ -14,4 +14,19 @@ class ZELTINA_UNREALCPP_API AFloatingActor_Curve : public AFloatingActor
 {
 	GENERATED_BODY()
 	
+protected:
+	// 스테틱매시 움직임 업데이트하는 함수(기본적으로 커브를 사용하고 커브가 없으면 부모 코드 사용)
+	virtual void OnFloatingMeshUpdate(float DeltaTime);
+
+public:
+
+	// 높이 움직임을 결정할 커브
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UCurveFloat> HeightCurve = nullptr;
+
+	// 커브 한 주기 동안의 시간
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Duration = 5.0f;
+
+
 };
