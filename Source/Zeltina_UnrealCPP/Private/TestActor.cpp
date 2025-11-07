18d42c0e15198f5c6737ca162d0ad47f82f1e395
zeltina99 <minec369@outlook.com>
2025-11-07 11:08:28 +0900

    로그 출력

--- Source/Zeltina_UnrealCPP/Private/TestActor.cpp
+++ Source/Zeltina_UnrealCPP/Private/TestActor.cpp
@@ -7,14 +7,19 @@
 ATestActor::ATestActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
 // Called when the game starts or when spawned
 void ATestActor::BeginPlay()
 {
-	Super::BeginPlay();
+	// Super = 부모 클래스
+	Super::BeginPlay(); // 부모 클래스의 BeginPlay를 실행해라.
+
+	UE_LOG(LogTemp, Log, TEXT("Hello Unreal. 한글도 됨. 일반 로그."));
+	UE_LOG(LogTemp, Warning, TEXT("Hello Unreal. 한글도 됨. 경고 로그."));
+	UE_LOG(LogTemp, Error, TEXT("Hello Unreal. 한글도 됨. 에러 로그."));
 	
 }
 
